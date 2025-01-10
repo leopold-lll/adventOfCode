d113ac05fc2ec0cf002a1a24fc75582a69dc0d49
Stefano Leonardi <stefano.leonardi@pervoice.it>
2025-01-10 15:41:54 +0100

    Add day09 part2 the sample work, the input fail.

--- AOC_2024/day09_Disk_Fragmenter/day09.py
+++ AOC_2024/day09_Disk_Fragmenter/day09.py
@@ -7,37 +7,74 @@ def read_input(file_path):
         digits = file.read().strip()
     # print(digits)
 
-    files=[]
+    disk, spaces, files=[], [], []
     for i in range(len(digits)):
         d = digits[i]
-        id = "." if (i+1) % 2 == 0 else str((i+1) // 2)
+        if (i+1) % 2 == 0:
+            id = "."
+            spaces.append([id, len(disk), int(d)])
+            # print(f"New space: {spaces[-1]}")
+        else:
+            id = str((i+1) // 2)
+            files.append([id, len(disk), int(d)])
+            # print(f"New file:  {files[-1]}")
+
         # print(f"Index: {i}, Digit: {d}, ID: {id}")
         new_list = [id] * int(d)
-        files.extend(new_list)
+        disk.extend(new_list)
         
-    return(files)
+    return(disk, spaces, files)
 
-def collapse_files(files):
+def collapse_disk_byBlock(disk):
     left = 0
-    right = len(files) - 1
+    right = len(disk) - 1
 
     while left < right:
-        if files[left] == ".":
-            while right > left and files[right] == ".":
+        if disk[left] == ".":
+            while right > left and disk[right] == ".":
                 right -= 1
             if right > left:
-                files[left], files[right] = files[right], files[left]
+                disk[left], disk[right] = disk[right], disk[left]
         left += 1
 
-    return(files)
+    return(disk)
+
+def collapse_disk_byFile(disk, spaces, files):
+    for file in reversed(files):
+        file_id, file_start, file_length = file
+        for space in spaces:
+            space_id, space_start, space_length = space
+            if space_length >= file_length:
+                # Move file to the space
+                for i in range(file_length):
+                    disk[space_start + i] = file_id
+                    disk[file_start + i] = space_id
+                # Update space
+                space[1] += file_length
+                space[2] -= file_length
+                break
+        # print("Moved file:", file_id, "".join(disk))  # Print the disk for every file analyzed
+    return disk
+
+def compute_sum(disk):
+    return sum(int(value) * index for index, value in enumerate(disk) if value != ".")
 
 def main():
-    files = read_input(get_file_path("input.txt")) # 'sample.txt' 'input.txt'
-    # print("".join(files))
-    files = collapse_files(files)
-    # print("".join(files))
-    total_sum = sum(int(value) * index for index, value in enumerate(files) if value != ".")
-    print(f"Day 09 - Part 1: {total_sum}")
+    disk1, spaces, files = read_input(get_file_path("sample.txt")) # 'sample.txt' 'input.txt'
+    disk2 = disk1.copy()
+    # Part 1
+    # print("".join(disk1))
+    disk1 = collapse_disk_byBlock(disk1)
+    # print("".join(disk1))
+    print(f"Day 09 - Part 1: {compute_sum(disk1)}")
+
+    # Part 2 (solution fail for the real input even if it solve the sample...)
+    # print("".join(disk2))
+    disk2 = collapse_disk_byFile(disk2, spaces, files)
+    print("".join(disk2))
+    print(f"Day 09 - Part 2: {compute_sum(disk2)}")
+
+
 
 
 if __name__ == "__main__":
